c3556f8c3bd28911b45f1db51c1ba63ebc189e38
whizzylee15 <wizzylee839@gmail.com>
2025-11-09 00:13:59 +0100

    Update settings.js for Whizzy

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~gYOfin",
+  OWNER_NUMBER: "+994405078672", 
+  PREFIX: "+",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
